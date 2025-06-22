416d4eab07c4e672e92f2077cc55c844707666be
gasweld <gasweld@gmail.com>
2025-06-22 23:17:05 +0300

    Accept script path as argument

--- lisp.c
+++ lisp.c
@@ -3,11 +3,26 @@
 #import tok.c
 #import inter.c
 
-int main() {
+int main(int argc, char *argv[]) {
+	tokenizer.t *b = NULL;
+	switch (argc) {
+		case 1: { b = tokenizer.stdin(); }
+		case 2: {
+			FILE *f = fopen(argv[1], "r");
+			if (!f) {
+				fprintf(stderr, "failed to open %s\n", argv[1]);
+				return 1;
+			}
+			b = tokenizer.file(f);
+		}
+		default: {
+			fprintf(stderr, "arguments: [<file>]\n");
+			return 1;
+		}
+	}
+
 	char buf[4096];
 	inter.t *in = inter.new();
-
-	tokenizer.t *b = tokenizer.stdin();
 	while (true) {
 		// Read a form.
 		tok.tok_t *x = read.read(b);
